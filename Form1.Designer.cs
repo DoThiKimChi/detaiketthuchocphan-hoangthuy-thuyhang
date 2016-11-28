18414f9fca523db7d0bd5472e819e41f7fe78197
hoangthuy <hoangthithuypk81@gmail.com>
2016-11-28 23:11:58 +0700

    thi hoc ki

---
+++ Form1.Designer.cs
@@ -0,0 +1,310 @@
+﻿namespace baithi
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.lbldiem = new System.Windows.Forms.Label();
+            this.lblmucchoi = new System.Windows.Forms.Label();
+            this.lbl_diem = new System.Windows.Forms.Label();
+            this.lbl_mucchoi = new System.Windows.Forms.Label();
+            this.lbltime = new System.Windows.Forms.Label();
+            this.lbl_time = new System.Windows.Forms.Label();
+            this.lbltamdung = new System.Windows.Forms.Label();
+            this.lblchoimoi = new System.Windows.Forms.Label();
+            this.lblthoat = new System.Windows.Forms.Label();
+            this.pb2 = new System.Windows.Forms.PictureBox();
+            this.pb3 = new System.Windows.Forms.PictureBox();
+            this.pb6 = new System.Windows.Forms.PictureBox();
+            this.pb9 = new System.Windows.Forms.PictureBox();
+            this.pb5 = new System.Windows.Forms.PictureBox();
+            this.pb8 = new System.Windows.Forms.PictureBox();
+            this.pb7 = new System.Windows.Forms.PictureBox();
+            this.pb4 = new System.Windows.Forms.PictureBox();
+            this.pb1 = new System.Windows.Forms.PictureBox();
+            this.pbmain = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbmain)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // lbldiem
+            // 
+            this.lbldiem.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbldiem.ForeColor = System.Drawing.Color.Red;
+            this.lbldiem.Location = new System.Drawing.Point(6, 25);
+            this.lbldiem.Name = "lbldiem";
+            this.lbldiem.Size = new System.Drawing.Size(89, 42);
+            this.lbldiem.TabIndex = 14;
+            this.lbldiem.Text = "Điểm:";
+            // 
+            // lblmucchoi
+            // 
+            this.lblmucchoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblmucchoi.ForeColor = System.Drawing.Color.Red;
+            this.lblmucchoi.Location = new System.Drawing.Point(6, 67);
+            this.lblmucchoi.Name = "lblmucchoi";
+            this.lblmucchoi.Size = new System.Drawing.Size(132, 42);
+            this.lblmucchoi.TabIndex = 15;
+            this.lblmucchoi.Text = "Mức chơi:";
+            // 
+            // lbl_diem
+            // 
+            this.lbl_diem.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_diem.ForeColor = System.Drawing.Color.Black;
+            this.lbl_diem.Location = new System.Drawing.Point(123, 25);
+            this.lbl_diem.Name = "lbl_diem";
+            this.lbl_diem.Size = new System.Drawing.Size(89, 42);
+            this.lbl_diem.TabIndex = 16;
+            this.lbl_diem.Text = "điểm";
+            // 
+            // lbl_mucchoi
+            // 
+            this.lbl_mucchoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_mucchoi.ForeColor = System.Drawing.Color.Black;
+            this.lbl_mucchoi.Location = new System.Drawing.Point(129, 67);
+            this.lbl_mucchoi.Name = "lbl_mucchoi";
+            this.lbl_mucchoi.Size = new System.Drawing.Size(126, 42);
+            this.lbl_mucchoi.TabIndex = 17;
+            this.lbl_mucchoi.Text = "mức chơi";
+            // 
+            // lbltime
+            // 
+            this.lbltime.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltime.ForeColor = System.Drawing.Color.Red;
+            this.lbltime.Location = new System.Drawing.Point(6, 118);
+            this.lbltime.Name = "lbltime";
+            this.lbltime.Size = new System.Drawing.Size(132, 42);
+            this.lbltime.TabIndex = 18;
+            this.lbltime.Text = "Thời gian";
+            // 
+            // lbl_time
+            // 
+            this.lbl_time.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbl_time.ForeColor = System.Drawing.Color.Black;
+            this.lbl_time.Location = new System.Drawing.Point(129, 118);
+            this.lbl_time.Name = "lbl_time";
+            this.lbl_time.Size = new System.Drawing.Size(126, 42);
+            this.lbl_time.TabIndex = 19;
+            this.lbl_time.Text = "thời gian";
+            // 
+            // lbltamdung
+            // 
+            this.lbltamdung.BackColor = System.Drawing.Color.DodgerBlue;
+            this.lbltamdung.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lbltamdung.ForeColor = System.Drawing.Color.Black;
+            this.lbltamdung.Location = new System.Drawing.Point(12, 370);
+            this.lbltamdung.Name = "lbltamdung";
+            this.lbltamdung.Size = new System.Drawing.Size(101, 32);
+            this.lbltamdung.TabIndex = 20;
+            this.lbltamdung.Text = "Tạm Dừng";
+            // 
+            // lblchoimoi
+            // 
+            this.lblchoimoi.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lblchoimoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblchoimoi.ForeColor = System.Drawing.Color.Black;
+            this.lblchoimoi.Location = new System.Drawing.Point(136, 370);
+            this.lblchoimoi.Name = "lblchoimoi";
+            this.lblchoimoi.Size = new System.Drawing.Size(88, 32);
+            this.lblchoimoi.TabIndex = 21;
+            this.lblchoimoi.Text = "Chơi Mới";
+            // 
+            // lblthoat
+            // 
+            this.lblthoat.BackColor = System.Drawing.SystemColors.Highlight;
+            this.lblthoat.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblthoat.ForeColor = System.Drawing.Color.Black;
+            this.lblthoat.Location = new System.Drawing.Point(76, 406);
+            this.lblthoat.Name = "lblthoat";
+            this.lblthoat.Size = new System.Drawing.Size(94, 42);
+            this.lblthoat.TabIndex = 22;
+            this.lblthoat.Text = "Thoát";
+            // 
+            // pb2
+            // 
+            this.pb2.Location = new System.Drawing.Point(392, 25);
+            this.pb2.Name = "pb2";
+            this.pb2.Size = new System.Drawing.Size(125, 125);
+            this.pb2.TabIndex = 13;
+            this.pb2.TabStop = false;
+            // 
+            // pb3
+            // 
+            this.pb3.InitialImage = global::baithi.Properties.Resources.DRMN_03;
+            this.pb3.Location = new System.Drawing.Point(523, 25);
+            this.pb3.Name = "pb3";
+            this.pb3.Size = new System.Drawing.Size(125, 125);
+            this.pb3.TabIndex = 12;
+            this.pb3.TabStop = false;
+            // 
+            // pb6
+            // 
+            this.pb6.Location = new System.Drawing.Point(523, 156);
+            this.pb6.Name = "pb6";
+            this.pb6.Size = new System.Drawing.Size(125, 125);
+            this.pb6.TabIndex = 11;
+            this.pb6.TabStop = false;
+            // 
+            // pb9
+            // 
+            this.pb9.Location = new System.Drawing.Point(523, 287);
+            this.pb9.Name = "pb9";
+            this.pb9.Size = new System.Drawing.Size(125, 125);
+            this.pb9.TabIndex = 10;
+            this.pb9.TabStop = false;
+            // 
+            // pb5
+            // 
+            this.pb5.Location = new System.Drawing.Point(392, 156);
+            this.pb5.Name = "pb5";
+            this.pb5.Size = new System.Drawing.Size(125, 125);
+            this.pb5.TabIndex = 9;
+            this.pb5.TabStop = false;
+            // 
+            // pb8
+            // 
+            this.pb8.Location = new System.Drawing.Point(392, 287);
+            this.pb8.Name = "pb8";
+            this.pb8.Size = new System.Drawing.Size(125, 125);
+            this.pb8.TabIndex = 8;
+            this.pb8.TabStop = false;
+            // 
+            // pb7
+            // 
+            this.pb7.Location = new System.Drawing.Point(261, 287);
+            this.pb7.Name = "pb7";
+            this.pb7.Size = new System.Drawing.Size(125, 125);
+            this.pb7.TabIndex = 7;
+            this.pb7.TabStop = false;
+            // 
+            // pb4
+            // 
+            this.pb4.Location = new System.Drawing.Point(261, 156);
+            this.pb4.Name = "pb4";
+            this.pb4.Size = new System.Drawing.Size(125, 125);
+            this.pb4.TabIndex = 6;
+            this.pb4.TabStop = false;
+            // 
+            // pb1
+            // 
+            this.pb1.Location = new System.Drawing.Point(261, 25);
+            this.pb1.Name = "pb1";
+            this.pb1.Size = new System.Drawing.Size(125, 125);
+            this.pb1.TabIndex = 5;
+            this.pb1.TabStop = false;
+            // 
+            // pbmain
+            // 
+            this.pbmain.Image = ((System.Drawing.Image)(resources.GetObject("pbmain.Image")));
+            this.pbmain.Location = new System.Drawing.Point(12, 163);
+            this.pbmain.Name = "pbmain";
+            this.pbmain.Size = new System.Drawing.Size(200, 200);
+            this.pbmain.TabIndex = 0;
+            this.pbmain.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(674, 457);
+            this.Controls.Add(this.lblthoat);
+            this.Controls.Add(this.lblchoimoi);
+            this.Controls.Add(this.lbltamdung);
+            this.Controls.Add(this.lbl_time);
+            this.Controls.Add(this.lbltime);
+            this.Controls.Add(this.lbl_mucchoi);
+            this.Controls.Add(this.lbl_diem);
+            this.Controls.Add(this.lblmucchoi);
+            this.Controls.Add(this.lbldiem);
+            this.Controls.Add(this.pb2);
+            this.Controls.Add(this.pb3);
+            this.Controls.Add(this.pb6);
+            this.Controls.Add(this.pb9);
+            this.Controls.Add(this.pb5);
+            this.Controls.Add(this.pb8);
+            this.Controls.Add(this.pb7);
+            this.Controls.Add(this.pb4);
+            this.Controls.Add(this.pb1);
+            this.Controls.Add(this.pbmain);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load_1);
+            ((System.ComponentModel.ISupportInitialize)(this.pb2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb6)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb9)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb8)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb7)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pb1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pbmain)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pbmain;
+        private System.Windows.Forms.PictureBox pb1;
+        private System.Windows.Forms.PictureBox pb4;
+        private System.Windows.Forms.PictureBox pb7;
+        private System.Windows.Forms.PictureBox pb8;
+        private System.Windows.Forms.PictureBox pb5;
+        private System.Windows.Forms.PictureBox pb9;
+        private System.Windows.Forms.PictureBox pb6;
+        private System.Windows.Forms.PictureBox pb3;
+        private System.Windows.Forms.Label lbldiem;
+        private System.Windows.Forms.Label lblmucchoi;
+        private System.Windows.Forms.Label lbl_diem;
+        private System.Windows.Forms.Label lbl_mucchoi;
+        private System.Windows.Forms.Label lbltime;
+        private System.Windows.Forms.Label lbl_time;
+        private System.Windows.Forms.Label lbltamdung;
+        private System.Windows.Forms.Label lblchoimoi;
+        private System.Windows.Forms.Label lblthoat;
+        private System.Windows.Forms.PictureBox pb2;
+
+        public System.EventHandler lbldiem_Click { get; set; }
+
+        public System.EventHandler Form1_Load { get; set; }
+
+        public System.EventHandler pbt1_Click { get; set; }
+
+        
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+
+namespace baithi
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private int x, y;
+        private int[] Arr = new int[9];
+        private int diem;
+        private int Time;
+        private int Level;
+        private bool TT_game = true;
+
+        private void initArr()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Arr[i - 1] = i;
+            }
+        }
+        private void init()
+        {
+            x = 2;
+            y = 2;
+            diem = 0;
+            Time = 0;
+            Level = 1;
+            initArr();
+
+        }
+        private int get(int x, int y)
+        {
+            return x + 3 * y;
+        }
+        private void getanh(PictureBox pb, int a)
+        {
+            switch (a)
+            {
+                case 1: pb.Image = baithi.Properties.Resources.DRMN_01; break;
+                case 2: pb.Image = baithi.Properties.Resources.DRMN_02; break;
+                case 3: pb.Image = baithi.Properties.Resources.DRMN_03; break;
+                case 4: pb.Image = baithi.Properties.Resources.DRMN_04; break;
+                case 5: pb.Image = baithi.Properties.Resources.DRMN_05; break;
+                case 6: pb.Image = baithi.Properties.Resources.DRMN_06; break;
+                case 7: pb.Image = baithi.Properties.Resources.DRMN_07; break;
+                case 8: pb.Image = baithi.Properties.Resources.DRMN_08; break;
+                case 9: pb.Image = null; pb.BackColor = Color.White; break;
+            }
+        }
+        private void showPb()
+        {
+            getanh(pb1, Arr[0]);
+            getanh(pb2, Arr[1]);
+            getanh(pb3, Arr[2]);
+            getanh(pb4, Arr[3]);
+            getanh(pb5, Arr[4]);
+            getanh(pb6, Arr[5]);
+            getanh(pb7, Arr[6]);
+            getanh(pb8, Arr[7]);
+            getanh(pb9, Arr[8]);
+        }
+        private void Form1_Load_1(object sender, EventArgs e)
+        {
+            init();
+            setmap();
+        }
+        private void intmap()
+        {
+            for (int i = 0; i < 200; i++) ;
+        }
+
+        private void go_left()
+        {
+            if (x > 0)
+            {
+                int temp = Arr[get(x, y)];
+                Arr[get(x, y)] = Arr[get(x - 1, y)];
+                Arr[get(x - 1, y)] = temp;
+                x--;
+                diem++;
+
+                showPb();
+            }
+        }
+        private void go_right()
+        {
+            if (x < 2)
+            {
+                int temp = Arr[get(x, y)];
+                Arr[get(x, y)] = Arr[get(x + 1, y)];
+                Arr[get(x + 1, y)] = temp;
+                x--;
+                diem++;
+
+                showPb();
+            }
+        }
+        private void go_top()
+        {
+            if (y > 0)
+            {
+                int temp = Arr[get(x, y)];
+                Arr[get(x, y)] = Arr[get(x, y - 1)];
+                Arr[get(x, y - 1)] = temp;
+                y--;
+                diem++;
+
+                showPb();
+            }
+        }
+        private void go_bottom()
+        {
+            if (y < 2)
+            {
+                int temp = Arr[get(x, y)];
+                Arr[get(x, y)] = Arr[get(x, y + 1)];
+                Arr[get(x, y + 1)] = temp;
+                y--;
+                diem++;
+
+                showPb();
+            }
+        }
+        private void setmap()
+        {
+            for (int i = 0; i < 200; i++)
+            {
+                Random rd = new Random();
+                int rdn = rd.Next(1, 5);
+
+                switch (rdn)
+                {
+                    case 1: go_top(); break;
+                    case 2: go_right(); break;
+                    case 3: go_left(); break;
+                    case 4: go_bottom(); break;
+                }
+            }
+            int temp = Arr[get(x, y)];
+            Arr[get(x, y)] = Arr[8];
+            Arr[8] = temp;
+            x = 2;
+            y = 2;
+            showPb();
+        
+        }
+    }
+}
